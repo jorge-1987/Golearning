2bd592303c2218cd94f14309d44c0faaad0df490
Jorge Abreu <jorgeabreu87@gmail.com>
2019-11-23 21:31:40 -0300

    Added to check ram memory in linux

--- api/webapi.go
+++ api/webapi.go
@@ -1,33 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
-
-    log.Fatal(http.ListenAndServe(":8080", router))
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
+
+	fmt.Fprintln(w, "<h1>General statistics:</h1>")
+	fmt.Fprintln(w, "</body></html>")
 }
 
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Todo Index!")
+func RamIndex(w http.ResponseWriter, r *http.Request) {
+	dat, err := ioutil.ReadFile("/proc/meminfo")
+	check(err)
+
+	fmt.Fprintln(w, "<html><head></head><body>")
+	fmt.Fprintln(w, "<h1>RAM statistics:</h1><br />")
+	fmt.Fprintln(w, "<p>")
+	fmt.Fprintln(w, string(dat))
+	//fmt.Print(string(dat))
+	fmt.Fprintln(w, "</p>")
+	fmt.Fprintln(w, "</body></html>")
 }
 
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
+func DiskShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	mount := vars["mount"]
+	fmt.Fprintln(w, "Disk show:", mount)
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/ram", RamIndex)
+	router.HandleFunc("/disk/{mount}", DiskShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
