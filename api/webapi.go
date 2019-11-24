afe4f2e332eb0aefd39c382f64bfcfcb960b6a2d
Jorge Abreu <jorgeabreu87@gmail.com>
2019-11-23 22:32:35 -0300

    ram info works

--- api/webapi.go
+++ api/webapi.go
@@ -28,44 +28,27 @@ func RamShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	typeram := vars["type"]
 
-	//dat, err := ioutil.ReadFile("/proc/meminfo")
-	//check(err)
-
 	dat, err := os.Open("/proc/meminfo")
 	check(err)
-	//	splitdat := strings.Split(string(dat), " ")
 
 	scanner := bufio.NewScanner(dat)
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), "MemFree:") {
-			fmt.Println(scanner.Text())
-		}
-		//fmt.Println(scanner.Text())
-	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 	memstring := ""
 
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), typeram) {
-			//fmt.Println(scanner.Text())
 			memstring = string(scanner.Text())
 		}
-		//fmt.Println(scanner.Text())
+
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintln(w, "<html><head></head><body>")
-	fmt.Fprintln(w, "<h1>RAM statistics:</h1><br />")
-	fmt.Fprintln(w, "<p>")
-	fmt.Fprintln(w, memstring)
-	fmt.Fprintln(w, "</p>")
-	fmt.Fprintln(w, "</body></html>")
+	page := "<html><head></head><body><h1>RAM statistics:</h1><br /><p>" + memstring + "</p></body></html>"
+
+	fmt.Fprintln(w, page)
 }
 
 func RamIndex(w http.ResponseWriter, r *http.Request) {
